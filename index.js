20ad41e431a6954f3e76815a206159e39723eb15
SeulgiSeong <hjlsgj@gmail.com>
2018-03-02 00:13:13 +0900

    subscription

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+const { createServer } = require("http");
 const {readFileSync} = require('fs');
 const bodyParser = require('body-parser');
 const {graphqlExpress, graphiqlExpress} = require('apollo-server-express');
 const {makeExecutableSchema} = require('graphql-tools');
 const express = require('express');
-const app = express();
+const {execute, subscribe} = require('graphql');
+const {SubscriptionServer} = require('subscriptions-transport-ws');
+const {PubSub} = require('graphql-subscriptions');
 
+const pubsub = new PubSub();
+const getStats = () => ({
+    salads: salads.length,
+    burgers: burgers.length
+});
 const get = (what, count) => what.splice(0, parseInt(count) || 1);
 const salad = { avocado: 1, mango: 1, tomato: 0.2, arugula: true, onion: true };
 const burger = { buns: 2, shrimp: 1, egg: 1, lettuce: 2.5, mayo: true };
@@ -20,16 +28,25 @@ const schema = makeExecutableSchema({
         Mutation: {
             addSalads: (_, {count}) => {
                 salads.push(...new Array(count).fill(salad));
+                pubsub.publish('foodAdded', {foodAdded: getStats()});
                 return salads.length;
             },
             addBurgers: (_, {count}) => {
                 burgers.push(...new Array(count).fill(burger));
+                pubsub.publish('foodAdded', {foodAdded: getStats()});
                 return burgers.length;
+            }
+        },
+        Subscription: {
+            foodAdded: {
+                subscribe: () => 
+                    pubsub.asyncInterator('foodAdded')
             }
         }
     }
 });
 const persistedQueries = require('./persisted-queries');
+const app = express();
 
 app.get('/salads', ({ query: { count } }, res) => 
     res.json(get(salads, count)));
@@ -48,6 +65,15 @@ app.use('/graphql', bodyParser.json(), (req, res, next) => {
     next();
 })
 app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
-app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
+app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql',
+    subscriptionsEndpoint: 'ws://localhost:4000/subscriptions'
+}));
 
-app.listen(4000);
+const server = createServer(app);
+server.listen(4000);
+
+new SubscriptionServer(
+    {schema, execute, subscribe},
+    {path: '/subscriptions', server}
+);
